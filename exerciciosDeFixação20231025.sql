543ff5e024851216314c1c49f78e3ea4b1a1b671
10042006-tech <98918134+10042006-tech@users.noreply.github.com>
2023-10-25 22:23:27 -0300

    Update exerciciosDeFixação20231025.sql

--- exerciciosDeFixação20231025.sql
+++ exerciciosDeFixação20231025.sql
@@ -4,27 +4,49 @@ CREATE DATABASE exercicios_trigger;
 USE exercicios_trigger;
 
 
+CREATE TRIGGER insere_auditoria
+AFTER INSERT ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem) VALUES ('Novo cliente inserido em ' || datetime('now'));
+END;
+//
+    
+CREATE TRIGGER tentativa_exclusao_cliente_auditoria
+BEFORE DELETE ON Clientes
+FOR EACH ROW
+BEGIN
+    INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de exclusão de cliente em ' || datetime('now'));
+END;
+//
+CREATE TRIGGER atualiza_nome_cliente_auditoria
+AFTER UPDATE ON Clientes
+FOR EACH ROW
+BEGIN
+    IF NEW.nome != OLD.nome AND (NEW.nome IS NOT NULL AND NEW.nome != '') THEN
+        INSERT INTO Auditoria (mensagem) VALUES ('Nome do cliente atualizado de ' || OLD.nome || ' para ' || NEW.nome || ' em ' || datetime('now'));
+    END IF;
+END;
+//
 
-CREATE TABLE Clientes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL
-);
+CREATE TRIGGER impedir_nome_vazio_null
+BEFORE UPDATE ON Clientes
+FOR EACH ROW
+BEGIN
+    IF NEW.nome IS NULL OR NEW.nome = '' THEN
+        INSERT INTO Auditoria (mensagem) VALUES ('Tentativa de atualização do nome para um valor vazio ou NULL em ' || datetime('now'));
+        SELECT RAISE(ABORT, 'O nome do cliente não pode ser vazio ou NULL');
+    END IF;
+END;
+//
 
-CREATE TABLE Auditoria (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    mensagem TEXT NOT NULL,
-    data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE Produtos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
-    estoque INT NOT NULL
-);
-
-CREATE TABLE Pedidos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    produto_id INT,
-    quantidade INT NOT NULL,
-    FOREIGN KEY (produto_id) REFERENCES Produtos(id)
-);
+CREATE TRIGGER decrementa_estoque
+AFTER INSERT ON Pedidos
+FOR EACH ROW
+BEGIN
+    UPDATE Produtos SET estoque = estoque - 1 WHERE id = NEW.produto_id;
+    IF (SELECT estoque FROM Produtos WHERE id = NEW.produto_id) < 5 THEN
+        INSERT INTO Auditoria (mensagem) VALUES ('Estoque do produto ' || (SELECT nome FROM Produtos WHERE id = NEW.produto_id) || ' abaixo de 5 unidades em ' || datetime('now'));
+    END IF;
+END;
+//
